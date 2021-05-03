a848d82b2daab6acc0e316b1a35d113a9d2b060f
Idoweiss-lab <idow@gilat.com>
2021-05-03 11:46:23 +0200

    Final code 3/5/21

--- TestForJob.py
+++ TestForJob.py
@@ -1,3 +1,8 @@
+FILE_PATH = 'P:\exam\\release_notes.txt'
+
+'''
+return the value [issue] Title number string for error with comment to the array
+'''
 def FunGitNote(arr , IndexInArray):
     Title = FileArray[IndexInArray].replace('Title: ', '')
     for line in arr[IndexInArray:]:
@@ -5,7 +10,9 @@ def FunGitNote(arr , IndexInArray):
            issue = line.replace('Issue: ','')
            issue = issue.replace("\n", "")
            return "["+issue+"]"+ " " + Title
-
+'''
+return the vaule [NOTE_MISSING] commit #number @owner , Please add git notes to this commit for missing comment on git
+'''
 def FunWithoutGitNote(arr , IndexInArray):
     NoteMissing = FileArray[num]
     for line in arr[IndexInArray:]:
@@ -14,25 +21,32 @@ def FunWithoutGitNote(arr , IndexInArray):
            owner = owner.replace("\n", "")
            return NoteMissing + "@"+owner+ " , Please add git notes to this commit!"
 
-CheckDuplicateEror = ""
-CheckOwnerNoCommaent = ["chdauto", "syspdbuild", "syspdbuild2"]
-GitNote = []
-WithoutGitNote = []
-FileArray = []
-f = open("P:\exam\\release_notes.txt", "r")
-for line in f:
-   FileArray.append(line)
+'''
+return the lines from file
+'''
+def load_file(path):
+    with open(path, "r") as f:
+        return f.readlines()
+
+CheckDuplicateEror = ""#array for check duplicate errors
+CheckOwnerNoCommaent = ["chdauto", "syspdbuild", "syspdbuild2"]#contains all the owner that dont need to print
+GitNote = []#array that contains all the errors with note
+WithoutGitNote = []#array that contains all the missing note
+FileArray = []#array that contains all the lines
+
+FileArray = load_file(FILE_PATH)
+
 ArrayLen = len(FileArray)
 for num in range(ArrayLen):
-    if("Title" in FileArray[num]):
-        GitNote.append(FunGitNote(FileArray,num))
-    elif ("[ NOTE_MISSING ]" in FileArray[num]):
-        WithoutGitNote.append(FunWithoutGitNote(FileArray,num))
+    if("Title" in FileArray[num]):#check if the string Ttile is in the line
+        GitNote.append(FunGitNote(FileArray,num))#add the string from the function to the array
+    elif ("[ NOTE_MISSING ]" in FileArray[num]):#check if the string [ NOTE_MISSING ] is in the line
+        WithoutGitNote.append(FunWithoutGitNote(FileArray,num))#add the string from the function
 for Error in GitNote:
-    if(Error not in CheckDuplicateEror):
+    if(Error not in CheckDuplicateEror):#check for duplicate errors
         print(Error)
-    CheckDuplicateEror += Error
+    CheckDuplicateEror += Error#add the error to the array
 for NoComment in WithoutGitNote:
-    if(NoComment.split("@")[1].split(" ,")[0] not in CheckOwnerNoCommaent):
+    if(NoComment.split("@")[1].split(" ,")[0] not in CheckOwnerNoCommaent):#check for owner that dont need to print
         print(NoComment)
 
