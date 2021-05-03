5670741c90806f827522fcfff13d647268dda65c
Idoweiss-lab <idow@gilat.com>
2021-05-03 13:05:51 +0200

    Final code 3/5/21

--- TestForJob.py
+++ TestForJob.py
@@ -1,25 +1,24 @@
 FILE_PATH = 'P:\exam\\release_notes.txt'
 
-'''
+"""
 return the value [issue] Title number string for error with comment to the array
-'''
-def FunGitNote(arr , IndexInArray):
-    Title = FileArray[IndexInArray].replace('Title: ', '')
-    for line in arr[IndexInArray:]:
-        if("Issue:" in line):
-           issue = line.replace('Issue: ','')
-           issue = issue.replace("\n", "")
-           return "["+issue+"]"+ " " + Title
+"""
+def FunGitNote(arr , idx):
+    title = arr[idx].replace('Title: ', '')
+    for line in arr[idx:]:
+        if "Issue:" in line:
+           issue = line.replace('Issue: ','').replace("\n", "")
+           return f'[{issue}] {title}'
+
 """
 return the vaule [NOTE_MISSING] commit #number @owner , Please add git notes to this commit for missing comment on git
 """
-def FunWithoutGitNote(arr , IndexInArray):
-    NoteMissing = FileArray[num]
-    for line in arr[IndexInArray:]:
+def FunWithoutGitNote(arr , index_in_array):
+    note_missing = arr[index_in_array]
+    for line in arr[index_in_array:]:
         if("Owner:" in line):
-           owner = line.replace('Owner: ','')
-           owner = owner.replace("\n", "")
-           return NoteMissing + "@"+owner+ " , Please add git notes to this commit!"
+           owner = line.replace('Owner: ','').replace("\n", "")
+           return f'{note_missing}@{owner}, Please add git notes to this commit!'
 
 """
 return the lines from file
@@ -31,37 +30,37 @@ def load_file(path):
 """
 print the error + title of the git notes
 """
-def summarize_git_note(git_note):
-    CheckDuplicateEror = ""  # array for check duplicate errors
-    for Error in GitNote:
-        if (Error not in CheckDuplicateEror):  # check for duplicate errors
-            print(Error)
-        CheckDuplicateEror += Error  # add the error to the array
+def print_git_note(git_note):
+    check_duplicate_error = ""  # array for check duplicate errors
+    for error in git_note:
+        if (error not in check_duplicate_error):  # check for duplicate errors
+            print(error)
+        check_duplicate_error += error  # add the error to the array
 
 """
 print the note missing + commit and the owner with request to add commit
 """
-def summarize_git_without_note(git_without_note):
-    CheckOwnerNoComment = ["chdauto", "syspdbuild", "syspdbuild2"]#owners that dont need to print the missing commit
-    for NoComment in WithoutGitNote:
-        if (NoComment.split("@")[1].split(" ,")[0] not in CheckOwnerNoComment):  # check for owner that dont need to print
-            print(NoComment)
+def print_git_without_note(git_without_note):
+    check_owner_no_comment = ["chdauto", "syspdbuild", "syspdbuild2"]#owners that dont need to print the missing commit
+    for no_comment in git_without_note:
+        if (no_comment.split("@")[1].split(",")[0] not in check_owner_no_comment):  # check for owner that dont need to print
+            print(no_comment)
 
 
-GitNote = []#array that contains all the errors with note
-WithoutGitNote = []#array that contains all the missing note
-FileArray = []#array that contains all the lines
+git_note = []#array that contains all the errors with note
+without_git_note = []#array that contains all the missing note
+file_array = []#array that contains all the lines
 
-FileArray = load_file(FILE_PATH)
+file_array = load_file(FILE_PATH)
 
-ArrayLen = len(FileArray)
+array_len = len(file_array)
 
-for num in range(ArrayLen):
-    if("Title" in FileArray[num]):#check if the string Ttile is in the line
-        GitNote.append(FunGitNote(FileArray,num))#add the string from the function to the array
-    elif ("[ NOTE_MISSING ]" in FileArray[num]):#check if the string [ NOTE_MISSING ] is in the line
-        WithoutGitNote.append(FunWithoutGitNote(FileArray,num))#add the string from the function
+for num in range(array_len):
+    if("Title" in file_array [num]):#check if the string Ttile is in the line
+        git_note.append(FunGitNote(file_array ,num))#add the string from the function to the array
+    elif ("[ NOTE_MISSING ]" in file_array [num]):#check if the string [ NOTE_MISSING ] is in the line
+        without_git_note.append(FunWithoutGitNote(file_array ,num))#add the string from the function
 
-summarize_git_note(GitNote)
-summarize_git_without_note(WithoutGitNote)
+print_git_note(git_note)
+print_git_without_note(without_git_note)
 
